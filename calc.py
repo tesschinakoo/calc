63c0a82ff07293d5eb0aed7c143cf63cda2c62d7
tesskoo <tkoo@chadwickschool.org>
2016-10-28 12:24:01 -0700

    finish interface and add 2-d list/nested list thing but not for the GUI

--- calc.py
+++ calc.py
@@ -15,14 +15,11 @@ Screen.outline = Color("Black")
 Screen.draw(win)
 
 
-#ADDITIONBUTTON
-x1addition = 85
-x2addition = 115
-y1addition = 95
-ButtonRectangle = Rectangle((85,95), (115,125))
-ButtonRectangle.fill = Color("lightGray")
-ButtonRectangle.outline = Color("Black")
-ButtonRectangle.draw(win)
+#ADDITION BUTTON
+SubtractionRectangle = Rectangle((85,95), (115,125))
+SubtractionRectangle.fill = Color("lightGray")
+SubtractionRectangle.outline = Color("Black")
+SubtractionRectangle.draw(win)
 VerticalPlus = Line((100,100), (100,120))
 VerticalPlus.fill = Color("Black")
 VerticalPlus.draw(win)
@@ -30,16 +27,59 @@ HorizontalPlus = Line((90, 110), (110, 110))
 HorizontalPlus.fill = Color("Black")
 HorizontalPlus.draw(win)
 
-#SUBTRACTIONBUTTON
-AdditionRectangle = Rectangle((180,95), (210,125))
-x1subtraction = 180
-x2subtraction = 210
-y1subtraction = 95
-y2subtraction = 125
-ButtonRectangle = Rectangle((180,95), (210,125))
-ButtonRectangle.fill = Color("lightGray")
-ButtonRectangle.outline = Color("Black")
-ButtonRectangle.draw(win)
-VerticalHorizontal = Line((185, 110), (205, 110))
+#SUBTRACTION BUTTON
+AdditionRectangle = Rectangle((185,95), (215,125))
+AdditionRectangle.fill = Color("lightGray")
+AdditionRectangle.outline = Color("Black")
+AdditionRectangle.draw(win)
+VerticalHorizontal = Line((190, 110), (210, 110))
 VerticalHorizontal.fill = Color("Black")
 VerticalHorizontal.draw(win)
+
+listcalc = [[1,2,3],[4,5,6],[7,8,9]]
+
+for i in range(len(listcalc)):
+    for j in range(len(listcalc)):
+        print(listcalc[i][j])
+
+#FIRST ROW NUMBER BUTTONS
+for x in range(0,3):
+    NumberButton = Rectangle((55 + (x * 80),145), (85 + (x * 80),175))
+    NumberButton.fill = Color("lightGray")
+    NumberButton.outline = Color("Black")
+    NumberButton.draw(win)
+for x in range(0,3):
+    Numbers = Text((70 + (x*80), 160), "1")
+    Numbers.fill = Color("black")
+    Numbers.draw(win)
+
+#SECOND ROW NUMBER BUTTONS
+for x in range(0,3):
+    NumberButton = Rectangle((55 + (x*80),195), (85 + (x * 80),225))
+    NumberButton.fill = Color("lightGray")
+    NumberButton.outline = Color("Black")
+    NumberButton.draw(win)
+for x in range(0,3):
+    Numbers = Text((70 + (x*80), 210), "1")
+    Numbers.fill = Color("black")
+    Numbers.draw(win)
+
+#THIRD ROW NUMBER BUTTONS
+for x in range(0,3):
+    NumberButton = Rectangle((55 + (x * 80),245), (85 + (x * 80),275))
+    NumberButton.fill = Color("lightGray")
+    NumberButton.outline = Color("Black")
+    NumberButton.draw(win)
+for x in range(0,3):
+    Numbers = Text((70 + (x*80), 260), "1")
+    Numbers.fill = Color("black")
+    Numbers.draw(win)
+    
+#EQUAL/ENTER BUTTON
+EnterRectangle = Rectangle((230,40), (285,75))
+EnterRectangle.fill = Color("lightGray")
+EnterRectangle.outline = Color("red")
+EnterRectangle.draw(win)
+EnterText = Text((258, 55), "Enter")
+EnterText.fill = Color("black")
+EnterText.draw(win)
